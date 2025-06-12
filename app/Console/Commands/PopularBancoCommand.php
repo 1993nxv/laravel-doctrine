186efe4ac68e44518ba1061b84e484895d8c8373
Delmondes <wellington.apk@gmail.com>
2025-06-12 14:52:31 -0300

    feat(seed): add seed para popular banco com dados de teste

---
+++ app/Console/Commands/PopularBancoCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Entities\Empresa;
+use App\Entities\Endereco;
+use App\Entities\Categoria;
+use Doctrine\ORM\EntityManagerInterface;
+use Illuminate\Console\Command;
+
+class PopularBancoCommand extends Command
+{
+    protected $signature = 'doctrine:seed';
+
+    protected $description = 'Popula o banco com dados de teste';
+
+    public function __construct(private EntityManagerInterface $em)
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $categoria1 = new Categoria();
+        $categoria1->setNome('Tecnologia');
+
+        $empresa = new Empresa();
+        $empresa->setNome('Acme LTDA');
+        $empresa->setEmail('contato@acme.com');
+        $empresa->setTelefone('11 99999-9999');
+        $empresa->addCategoria($categoria1);
+
+        $endereco = new Endereco();
+        $endereco->setLogradouro('Rua das Flores, N123, Goiânia-GO');
+        $empresa->addEndereco($endereco);
+
+        $this->em->persist($empresa);
+        $this->em->flush();
+
+        $this->info('Dados inseridos com sucesso!');
+    }
+}
